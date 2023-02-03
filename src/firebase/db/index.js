a5eb105df98cdde5c7bd37699a67e4ce09987c00
Julián Camilo García Rincón <jcgrrincon@gmail.com>
2023-02-03 12:54:45 -0500

    primer commit

---
+++ src/firebase/db/index.js
@@ -0,0 +1,12 @@
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import app from "..";
+
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, "cities");
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map((doc) => doc.data());
+  return cityList;
+}
---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDa_AHGMtxlY4oNckFTrjLxVxhhbRyd4MA",
+  authDomain: "menupproject.firebaseapp.com",
+  projectId: "menupproject",
+  storageBucket: "menupproject.appspot.com",
+  messagingSenderId: "724809229048",
+  appId: "1:724809229048:web:6c189fe6746951acfbd10d",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
