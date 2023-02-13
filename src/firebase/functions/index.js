f70092a981258371dc906fb735f75a4af1c0f16c
Julián Camilo García Rincón <jcgrrincon@gmail.com>
2023-02-13 05:17:32 -0500

    Deleted the tash files that were in the application

--- src/firebase/functions/index.js
+++
@@ -1,7 +0,0 @@
-import app from "..";
-import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
-
-const functions = getFunctions(app);
-connectFunctionsEmulator(functions, "localhost", 5001);
-
-export default functions;
