a5d9d9ba0bf5085c5f39a73c4c6b0fa90cd1eb73
Julián Camilo García Rincón <jcgrrincon@gmail.com>
2024-02-06 13:51:27 -0500

    i was fix some problems with the emulators

--- src/firebase/db/index.js
+++ src/firebase/db/index.js
@@ -1,6 +1,10 @@
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 import app from "..";
 
 const db = getFirestore(app);
+
+if (process.env.DEV) {
+  connectFirestoreEmulator(db, '127.0.0.1', 8080);
+}
 
 export default db;
